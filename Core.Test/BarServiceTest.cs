d9a7d09af079563b39e0f7cb3424385316c29de4
Ben Craig <ben.craig@sita.aero>
2020-07-16 16:15:14 +0100

    Example of automocker for testing.

---
+++ Core.Test/BarServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Core.Test
+{
+    public class BarServiceTest
+    {
+
+        [Fact]
+        public void ShouldCallIntoFooServiceTenTimes()
+        {
+            // Arrange
+            var automocker = new AutoMocker();
+            var fixture = automocker.CreateInstance<BarService>();
+
+            // Act
+            fixture.DoBarThing();
+
+            // Assert
+            var fooServiceMock = automocker.GetMock<IFooService>();
+            fooServiceMock.Verify(x => x.DoFooThing(It.IsAny<int>()), Times.Exactly(10));
+        }
+    }
+}
